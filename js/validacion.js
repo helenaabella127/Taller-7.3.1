575566edee24ceb8dadc58abc2deeaddcde8915e
smartinrr <sabrinamartinrey@gmail.com>
2024-08-15 19:13:36 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -6,10 +6,15 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-let password1 = getElementById("nombre").value;
-let password2 = getElementById("nombre").value;
+document.getElementById('regBtn').addEventListener('click', function () {
+let password1 = document.getElementById("password1").value.trim();
+let password2 = document.getElementById("password2").value.trim();
 
 if (password1 !== password2) {
     showAlertError();
     return;
+} else {
+    showAlertSuccess();
 }
+
+});
